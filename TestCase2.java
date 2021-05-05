1e4af88ef5e535614e0c75c45a4ea650415a8e2b
FATMA KOÇAK <42037891+fatmakocak@users.noreply.github.com>
2021-05-05 17:39:29 +0300

    Add files via upload

---
+++ TestCase2.java
@@ -0,0 +1,13 @@
+package TestsDeneme;
+
+public class TestCase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestCase testcase = new TestCase();
+		testcase.n11WebPageTestCases();
+
+	}
+
+}
+
